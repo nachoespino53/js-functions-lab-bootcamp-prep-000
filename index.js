7f5628ff4808dd3210b3135cbc4340afabd82d10
Ignacio R Espino <irespino@capozios.com>
2018-08-01 15:09:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+
+function happyHolidays() {
+  return "Happy Holidays!";
+}
+
+function happyHolidaysTo(name) {
+  return `Happy Holidays, ${name}!`;
+}
+
+const happyHolidayTo = (holiday, name) => {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+const holidayCountdown = (holiday, days) => {
+  return ` It's ${days} days until ${holiday}!`;
+}
